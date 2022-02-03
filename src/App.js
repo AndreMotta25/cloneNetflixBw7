5b2088f0d5e7d4113effd02997dadf2acabd208b
AndreMotta25 <relaie22@gmail.com>
2022-02-03 14:40:58 -0300

    terminando a interface do netflix

--- src/App.js
+++ src/App.js
@@ -2,25 +2,71 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
 import MovieRows from "./components/MovieRows/MovieRows";
-
+import FeaturedMovie from "./components/FeaturedMovie/FeaturedMovie";
+import Header from "./components/Header/Header";
 function App() {
+  const [featuredData, setFeaturedData] = useState(null);
   const [movieList, setMovieList] = useState([]);
+  const [blackHeader, setBlackHeader] = useState(false);
+  // busca os dados da api
   useEffect(() => {
     const loadAll = async () => {
       // pegando a lista total
       let listAll = await Tmdb.getHomeList();
       console.log(listAll);
       setMovieList(listAll);
+      // pegando o featured
+      let originals = listAll.filter((movie) => movie.slug === "originals");
+      /*
+        eu preferia usar o min e max, mas esse tambem serve, digamos que o random retorne 0.7, vamos 
+        multiplica-lo por 9 e teremos 6,3 e como vamos arrendondar teremos 6, o valor nunca sera maior que 9
+      */
+      let randomChosen = Math.floor(
+        Math.random() * originals[0].items.results.length - 1
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let choseInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+      setFeaturedData(choseInfo);
     };
     loadAll();
   }, []);
+  // remove o evento de scroll do windows
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+    window.addEventListener("scroll", scrollListener);
+    return () => window.removeEventListener("scroll", scrollListener);
+  }, []);
   return (
     <div className="page">
+      <Header black={blackHeader} />
+      {featuredData && <FeaturedMovie item={featuredData} />}
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRows key={key} title={item.title} items={item.items} />
         ))}
       </section>
+      <footer>
+        Feito com{" "}
+        <span role="img" aria-label="coracao">
+          ❤️
+        </span>
+        pela B7Web.
+        <br /> Direitos de imagem para netflix
+        <br />
+        Dados pego do site TheMoviedb.org
+      </footer>
+      {/* faz o carregamento */}
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/MovieRows/MovieRows.js
+++ src/components/MovieRows/MovieRows.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieRows.css";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 
 export default ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(0);
   console.log(items);
+  const handleLeftArrow = () => {
+    // vamos se basear na largura da tela do  usuario
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  };
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listW = items.results.length * 150;
+    if (window.innerWidth - listW > x) {
+      // o 60 e para descontar o padding que foi colocado na tela
+      x = window.innerWidth - listW - 60;
+    }
+    setScrollX(x);
+  };
+
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow--left" onClick={handleLeftArrow}>
+        <NavigateBeforeIcon style={{ fontSize: 50 }} />
+      </div>
+      <div className="movieRow--right" onClick={handleRightArrow}>
+        <NavigateNextIcon style={{ fontSize: 50 }} />
+      </div>
       <div className="movieRow--listArea">
-        {items.results.length > 0 &&
-          items.results.map((item, key) => (
-            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
-          ))}
+        {/* essa div aqui vai conter uma categoria */}
+        <div
+          className="movieRow--list"
+          style={{
+            marginLeft: scrollX,
+            width: items.results.length * 150,
+          }}
+        >
+          {items.results.length > 0 &&
+            items.results.map((item, key) => (
+              <div key={key} className="movieRow--item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
